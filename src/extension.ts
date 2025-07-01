150baa6d2e062e7d572d0db039082162eabdaac3
pihh <pihh.rocks@gmail.com>
2025-07-01 12:55:51 +0100

    xxx

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,15 @@
+/**
+ * Pihhthon Header Formatter
+ * Copyright (C) 2025 Filipe Sá
+ *
+ * This file is part of Pihhthon Header Formatter.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ */
+
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
